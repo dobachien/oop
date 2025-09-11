d476b791ea172c83f6845aec52a57aced4de8337
dobachien <bachiendo@gmail.com>
2025-09-11 09:19:54 +0700

    Update Program.cs

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,10 +1,8 @@
-﻿internal class Program
+using System;
+internal class Program
 {
-    
-    private static void Main(string[] args)
+ private static void Main(string[] args)
     {
-        MyDate md1 = new MyDate(DateTime.Now.Day,DateTime.Now.Month, DateTime.Now.Year);
-        Console.WriteLine("{0}/{1}/{2}",md1.Day,md1.Month,md1.Year);
-        Console.ReadKey();
+        Console.WriteLine("Đỗ Bá Chiến\t12424041\t12424TN\tdobachien\tbachiendo@gmail.com");
     }
 }

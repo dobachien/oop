5db6208b70bb36981cf73773a6ccfd40cc72ace7
dobachien <bachiendo@gmail.com>
2025-09-11 09:23:14 +0700

    Update Program.cs

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -3,6 +3,8 @@ internal class Program
 {
  private static void Main(string[] args)
     {
+        Console.InputEncoding=Encoding.UTF8;
+        Console.OutputEncoding=Encoding.UTF8;
         Console.WriteLine("Đỗ Bá Chiến\t12424041\t12424TN\tdobachien\tbachiendo@gmail.com");
     }
 }
